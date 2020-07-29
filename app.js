1c71bcadba2e4bbea7f9846a504c013f139f13b7
Gabriel <knuckles4x@gmail.com>
2020-07-28 21:11:25 -0300

    My first view using EJS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
+
 const port = 3333;
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res, next) => {
-  res.send({
+  res.render("index", {
     title: "Hello World",
     version: "0.0.0",
   });
