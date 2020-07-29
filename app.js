b5ff70f2990ffc9e5319d6efb13f347098c87179
Gabriel <knuckles4x@gmail.com>
2020-07-29 02:06:55 -0300

    routes and styles

--- app.js
+++ app.js
@@ -4,22 +4,10 @@ const path = require("path");
 const port = 3333;
 const app = express();
 
-const CurriculoController = require("./controllers/curriculo-controller");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-app.get("/", (req, res, next) => {
-  res.render("index", {
-    title: "Hello World",
-    version: "0.0.0",
-  });
-});
-
-app.get("/curriculo", (req, res, next) => {
-  const curriculoData = CurriculoController.getData();
-  res.render("curriculo", curriculoData);
-});
 
 app.listen(port, (err) => {
   console.log(`Server is listening on ${port}`);
